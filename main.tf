9789b3bf212029afbe2c31824eb736f67d0cb70f
c1nd3r3114 <hunghainhi2003@gmail.com>
2024-10-14 09:15:39 +0700

    Refactor VPC and subnet configuration

--- main.tf
+++ main.tf
@@ -1,86 +1,114 @@
-resource "aws_vpc" "main" {
+# Create VPC
+resource "aws_vpc" "terraform-vpc" {
   cidr_block = "10.0.0.0/16"
-
+  
   tags = {
-    Name = "main_vpc"
+    Name = "terraform_vpc"
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public_subnet"
-  }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+#Create subnet
+##Private Subnet 
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.terraform-vpc.id
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name = "private-subnet"
+  }
+}
+##Public Subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.terraform-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  #map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+#Create internet gateway
+resource "aws_internet_gateway" "terraform-igw" {
+  vpc_id = aws_vpc.terraform-vpc.id
+  tags = {
+    Name = "terraform-vpc-IGW"
+    }
+}
+
+#Create public route table
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.terraform-vpc.id
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+#Create public-route
+resource "aws_route" "public-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.terraform-igw.id
+}
+
+#Connect public subnet with route table
+resource "aws_route_table_association" "public-subnet-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+#Create Elastic IP
+resource "aws_eip" "nat-eip" {
+  tags = {
+    Name = "nat-eip"
+    }
+}
+#Create nat-gateway
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.nat-eip.id
+  subnet_id     = aws_subnet.public-subnet.id
+  tags = {
+      Name = "nat-gateway"
+      }
+}
+#Create private route table 
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.terraform-vpc.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
+#Create private-route
+resource "aws_route" "private-route" {
+  route_table_id         = aws_route_table.private-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat-gateway.id
+}
+#Connect private subnet with route table
+resource "aws_route_table_association" "private-subnet-association" {
+  subnet_id      = aws_subnet.private-subnet.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+#Create Default Security Group
+resource "aws_security_group" "default_sg" {
+  vpc_id = aws_vpc.terraform-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "private_subnet"
+    Name = "default_sg"
   }
 }
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main_igw"
-  }
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public_rt"
-  }
-}
-
-resource "aws_route_table_association" "public_rta" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public_subnet.id
-
-  tags = {
-    Name = "main_nat"
-  }
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "private_rt"
-  }
-}
-
-resource "aws_route_table_association" "private_rta" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.30.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-1" # Replace with your preferred AWS region
+  region = "us-east-1"
 }
